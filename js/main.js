c6dd15b5f2a25d44763488327f7f1a1574158afe
jicumollick <56165529+jicumollick@users.noreply.github.com>
2019-11-29 09:14:09 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// var firstName = prompt("what is your first name? ");
+
+// var lastName = prompt("what is your last name? ");
+
+ // var userAge =Number( prompt("what is guess number?"));
+
+// alert("you have lived for "+ userAge*356 + " days");
+
+// alert("your full name is " + firstName +" "+ lastName);
+
+// alert("you are "+ userAge +" years old");
+
+// console.log("your full name is " + firstName +" "+ lastName);
+
+// console.log("you are "+ userAge +" years old");
+
+// if(userAge===5) console.log("your guess is correct");
+// else if (userAge<5) console.log("your guess is too low");
+// else console.log("your guess is too high");
